91fec08e19ba5d769d5652301563809231fb7aa5
Rob Cary <r0bc4ry@gmail.com>
2022-06-06 09:31:46 -0400

    Work on adding blog posts to home page

---
+++ components/footer/index.js
@@ -0,0 +1,23 @@
+import { Box, Container, Typography } from "@mui/material";
+
+const Footer = ({ title, description }) => {
+  return (
+    <Box component="footer" sx={{ bgcolor: "background.paper", py: 6 }}>
+      <Container maxWidth="lg">
+        <Typography variant="h6" align="center" gutterBottom>
+          {title}
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="text.secondary"
+          component="p"
+        >
+          {description}
+        </Typography>
+      </Container>
+    </Box>
+  );
+};
+
+export default Footer;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,35 @@
-import '../styles/globals.css'
+import * as React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import "../styles/globals.css";
+import { ThemeProvider } from "@mui/material";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const theme = createTheme({
+  typography: {
+    fontFamily: "Raleway, Arial",
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: `
+        @font-face {
+          font-family: 'Raleway';
+          font-style: normal;
+          font-display: swap;
+          font-weight: 400;
+          src: local('Raleway'), local('Raleway-Regular'), url(${RalewayWoff2}) format('woff2');
+          unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
+        }
+      `,
+    },
+  },
+});
+
+function App({ Component, pageProps }) {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default App;
